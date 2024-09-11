5b312c0a286293ebb87d26a4288760ea505c4bdc
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-09-11 17:47:49 +0200

    10 email requests

--- jscript/script.js
+++ jscript/script.js
@@ -5,14 +5,26 @@ const { createApp } = Vue
     data() {
       return {
         emails: [
-          'primaemail@primaemail.com',
-          'secondaemail@secondaiemail.com',
-          'terzaemail@terzaemail.com'
         ],
-        apriUrl: '',
+        apriUrl:'https://flynn.boolean.careers/exercises/api/random/mail',
       }
     },
     methods: {
-
+      getRandomEmail () {
+        // 10 richieste di email
+        for (let i = 0; i < 10; i++) {
+          axios.get(this.apriUrl)
+          .then((response) => {
+            // Inserisco la parte di risposta che mi interessa in una const
+            const result = response.data.response;
+            console.log(result);
+            // Aggiungo il risultato in un array nei data di vue
+            this.emails.push(result);
+          })
+        }
+      },
+    },
+    beforeMount () {
+      this.getRandomEmail();
     }
   }).mount('#app')
